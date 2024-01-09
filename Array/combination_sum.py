b1b807bf1de5865e7f1754e2a1f2217b2209f08c
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-01-08 22:10:19 -0800

    39 combination sum

---
+++ Array/combination_sum.py
@@ -0,0 +1,32 @@
+# lc 39
+
+class Solution:
+    def combination_sum(self, candidates: list[int], target: int) -> list[list[int]]:
+        candidates.sort()
+
+        combination = []
+        result = []
+        self.find_combination_sum_helper(candidates, target, 0, combination, result)
+
+        return result
+
+    
+    def find_combination_sum_helper(self, candidates: list[int], remaining: int, start: int, combination: list[int], result: list[list[int]]) -> None:
+        n = len(candidates)
+        if start >= n or remaining < candidates[start]:
+            return
+        
+        for i in range(start, n):
+            if candidates[i] == remaining:
+                combination.append(candidates[i])
+                result.append(list(combination))
+                combination.pop()
+                return
+
+            # remaining > candidates[i]
+            combination.append(candidates[i])
+            self.find_combination_sum_helper(candidates, remaining - candidates[i], i, combination, result)
+            combination.pop()
+
+a = Solution()
+print(a.combination_sum([2,3,6,7], 7))
