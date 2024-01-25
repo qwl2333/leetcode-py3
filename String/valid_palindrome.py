8c63e0135259bf252627d732dfe1818d3467a57a
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-01-25 01:28:21 -0800

    125 valid palindrome

---
+++ String/valid_palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if l < r:
+                if s[l] != s[r]:
+                    return False
+                else:
+                    l += 1
+                    r -= 1
+        return True
+                
+s = Solution()
+print(s.isPalindrome('A man, a plan, a canal: Panama'))      
