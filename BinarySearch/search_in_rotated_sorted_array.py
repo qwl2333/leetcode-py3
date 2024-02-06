fd9e6fc86b276acb78951556793a02160544540f
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-02-05 16:24:06 -0800

    33. Search in Rotated Sorted Array

---
+++ BinarySearch/search_in_rotated_sorted_array.py
@@ -0,0 +1,23 @@
+# lc 33
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
+                return mid
+            if target >= nums[0]:
+                if nums[mid] > target or nums[mid] < nums[0]:
+                    r = mid - 1
+                else: # nums[mid] > target and nums[mid] >= nums[0]
+                    l = mid + 1
+            else:
+                if nums[mid] >= nums[0] or nums[mid] < target:
+                    l = mid + 1
+                else: # nums[mid] < nums[0] and nums[mid] > target
+                    r = mid - 1
+        return -1
+
+s = Solution()
+print(s.search([4,5,6,7,0,1,2], 0))
+        
