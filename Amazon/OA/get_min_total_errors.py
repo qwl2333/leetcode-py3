4d396a3d95ff6b97e9ccf9264888edf93bc8649b
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-02-25 11:25:00 -0800

    amazon oa

--- Amazon/OA/get_min_total_errors.py
+++ Amazon/OA/get_min_total_errors.py
@@ -9,6 +9,7 @@ class Solution:
 # If the ! is replaced with '1, the string is "10111". The subsequence 01 occurs 3 times and 10 occurs 1 time. The number of errors is 3 * x + y = 9.
 # The minimum number of errors is min(9, 15) modulo (10^9 +7) = 9.
     
+    # 方法一是前缀和，不太好
     def get_min_total_errors(self, error_string: str, x: int, y: int) -> int:
         n = len(error_string)
         left_one_count = [0 for _ in range(n + 1)]
@@ -34,15 +35,50 @@ class Solution:
                 one_zero_sub_count += left_one_count[i]
         
         return zero_one_sub_count * x + one_zero_sub_count * y
+    
+    # 方法二更好，因为是dfs backtracking找到所有组合，同时还会记录左边zero和one的数量
+    def get_all_possible_strings(self, input_error_string: str):
+        res = []
+        self.left_zero_count = 0
+        self.left_one_count = 0
+        self.zero_acc = 0
+        self.one_acc = 0
+        def dfs(i: int, path: str):
+            if len(path) > 0:
+                if path[-1] == '0':
+                    self.left_zero_count += 1
+                    self.one_acc += self.left_one_count # 每次遇到0，就要累计一下左边目前为止有多少1，说明新加了这么多的‘10’
+                else:
+                    self.left_one_count += 1
+                    self.zero_acc += self.left_zero_count # 每次遇到1，就要累计一下左边目前为止有多少0
+            if i >= len(input_error_string):
+                res.append(path)
+                return
+            
+            
+            if input_error_string[i] != '!':
+                dfs(i + 1, path + input_error_string[i])
+            else:
+                # 为什么这里不需要backtracking, 因为path是immutable的
+                # 每次进入下一层产生的string都是新的string
+                # 如果不用path，用list，因为list是mutable, 每次加了新的char之后还要主动remove掉
+                dfs(i + 1, path + '0')
+                dfs(i + 1, path + '1')
+        
+        dfs(0, '')
+        print(f'01sequences: {self.zero_acc}')
+        print(f'10sequences: {self.one_acc}')
+        return res
+
         
 
 s = Solution()
 input_error_string = '101!1'
-DIVISOR = 10 ** 9 + 7 # 除数（分子） divisor， 被除数（分母） dividend
-idx = input_error_string.find('!')
-replaced_by_one = input_error_string[:idx] + '0' + input_error_string[idx+1:]
-replaced_by_zero = input_error_string[:idx] + '1' + input_error_string[idx+1:]
-result1 = s.get_min_total_errors(replaced_by_zero, 2, 3)
-result2 = s.get_min_total_errors(replaced_by_one, 2, 3)
-res = min(result1, result2) % DIVISOR
-print(res)
+# DIVISOR = 10 ** 9 + 7 # 除数（分子） divisor， 被除数（分母） dividend
+# idx = input_error_string.find('!')
+# replaced_by_one = input_error_string[:idx] + '0' + input_error_string[idx+1:]
+# replaced_by_zero = input_error_string[:idx] + '1' + input_error_string[idx+1:]
+# result1 = s.get_min_total_errors(replaced_by_zero, 2, 3)
+# result2 = s.get_min_total_errors(replaced_by_one, 2, 3)
+# res = min(result1, result2) % DIVISOR
+print(s.get_all_possible_strings('10111'))
