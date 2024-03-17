9cd3239e6e4c303263d6ed08ee23e718995e9a70
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-03-16 23:31:21 -0700

    227. Basic Calculator II

---
+++ Meta/Stack/basic-calculator-ii.py
@@ -0,0 +1,29 @@
+# lc 227
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack, cur_num, last_op = [], 0, '+' # cur_num是记录连续数字的，起始值为0，默认的last_operator也是+, 为什么呢？
+                                              # 如果是‘3-2*2', 那么到'-'时因为cur_num初始为0，可以得到3，其他初始值得不到3, last_op='+', 会把3加到stack里面去
+                                              # 如果是‘-3-2*2’，那么到第一个'-'时，因为cur_num初始为0，last_op='+'，会把0加到stack里面去
+        for c in s + '+': # the last added operator could be any of + - * /, it aims to do one more loop and add value to the stack
+            if c == ' ':
+                continue
+
+            if c.isdigit():
+                cur_num = (cur_num * 10) + int(c)
+            else:
+                if last_op == '-':
+                    stack.append(-cur_num)
+                elif last_op == '+':
+                    stack.append(cur_num)
+                elif last_op == '*':
+                    stack.append(stack.pop() * cur_num)
+                elif last_op == '/':
+                    stack.append(int(stack.pop() / cur_num)) # 这个int()是truncate towards zero, 如果用//，floor division，那么-3//2=-2 而不是-1
+                cur_num, last_op = 0, c
+        return sum(stack)
+
+s = Solution()
+print(s.calculate('42'))
+
+# 但计算'3-2*2'
+# stack里面[3], [3, -2], [3, -4]，每次都是在operator的位置(+-*/)，计算上一个operator的值并放入到stack里面
