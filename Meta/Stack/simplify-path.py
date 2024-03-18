4dc850003bc7d3fa372d6b414926ef945c202b02
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-03-18 00:13:57 -0700

    71. Simplify Path

---
+++ Meta/Stack/simplify-path.py
@@ -0,0 +1,30 @@
+# lc 71
+class Solution:
+    # Time O(n), space O(n)
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        cur_dir = ''
+        for c in path + '/':
+            if c != '/':
+                cur_dir += c
+            else: # c = '/'
+                if cur_dir == '':
+                    continue
+                elif cur_dir == '.':
+                    cur_dir = ''
+                elif cur_dir == '..':
+                    if stack:
+                        stack.pop()
+                    cur_dir = ''
+                else: # cur_dir is a real directory could be a, AAA, ...A, etc
+                    stack.append(cur_dir)
+                    cur_dir = ''
+
+        res = ''
+        while stack:
+            res = stack.pop() + res
+            res = '/' + res
+        return res if res != '' else '/'
+
+s = Solution()
+print(s.simplifyPath('/..a/...b/../.')) # /..a
