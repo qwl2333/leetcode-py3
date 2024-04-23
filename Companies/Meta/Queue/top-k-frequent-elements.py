d92e9fb617dcb1873967819d555265c20ef17b62
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-04-23 13:52:32 -0500

    continue

---
+++ Companies/Meta/Queue/top-k-frequent-elements.py
@@ -0,0 +1,43 @@
+# lc 347
+from queue import PriorityQueue
+class Solution:
+    # worst case T: O(nlogn) 此时 n - len(nums), k = n， S: O(n)
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        pq = PriorityQueue()
+
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        
+        for num, f in freq.items():
+            pq.put((-f, num))
+        
+        res = []
+        for i in range(k):
+            res.append(pq.get()[1])
+        
+        return res
+
+    # Time O(n)  Space O(n)  n = len(nums)
+    def topKFrequentBucketSort(self, nums: list[int], k: int) -> list[int]:
+        bucket = [[] for _ in range(len(nums) + 1)]
+
+        freq_map = {}
+        for num in nums:
+            freq_map[num] = freq_map.get(num, 0) + 1
+        
+        for num, freq in freq_map.items():
+            bucket[freq].append(num)
+        
+        res = []
+        for i in range(len(nums), -1, -1):
+            if not bucket[i]:
+                continue
+            res.extend(bucket[i])
+            if len(res) == k:
+                break
+
+        return res
+
+s = Solution()
+print(s.topKFrequentBucketSort([1,1,1,2,2,3], 2))
