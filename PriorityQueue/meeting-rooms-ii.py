0be936b690a1d39d9fe8015596fa4f4a85d3f179
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-05-02 12:45:28 -0500

    Meeting Rooms II

---
+++ PriorityQueue/meeting-rooms-ii.py
@@ -0,0 +1,23 @@
+# lc 253
+from heapq import heappop, heappush
+class Solution:
+    # Time O(nlogn) n - len of intervals, space O(n) worst case all intervals in min_heap
+    def minMeetingRooms(self, intervals: list[list[int]]) -> int:
+        min_heap = []
+        room_counter = 0
+        intervals.sort(key=lambda i: i[0])
+        for s, e in intervals:
+            if not min_heap:
+                heappush(min_heap, e)
+                room_counter += 1
+                continue
+            if s < min_heap[0]:
+                room_counter += 1
+                heappush(min_heap, e)
+            else:
+                heappop(min_heap)
+                heappush(min_heap, e)
+        return room_counter
+
+s = Solution()
+print(s.minMeetingRooms([[0,30],[5,10],[5,10],[5,10],[15,20]])) # 4 rooms
