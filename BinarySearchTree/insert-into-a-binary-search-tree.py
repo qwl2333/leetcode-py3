40a2f5ee6457a264ba5e0a137efbd7a5d9835d4c
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-06-24 12:47:18 -0500

    Insert into a Binary Search Tree

---
+++ BinarySearchTree/insert-into-a-binary-search-tree.py
@@ -0,0 +1,19 @@
+# lc 701
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    # O(h), h is height of bst, ranges from logn to n
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
