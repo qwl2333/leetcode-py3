cadba1e9d9757766e6850fc35890d2a32a20c52a
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-06-24 13:37:24 -0500

    Number of 1 Bits

---
+++ BitManipulation/number-of-1-bits.py
@@ -0,0 +1,9 @@
+# lc 191
+class Solution:
+    # time 0(32) n is a 32 digit number
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n != 0:
+            count += n & 1
+            n = n >> 1
+        return count
