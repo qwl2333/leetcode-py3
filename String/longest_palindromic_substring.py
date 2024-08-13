b963bcf402020e30d08e0e30d76eff9b5c64547a
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2024-08-13 15:04:32 -0700

    review string

--- String/longest_palindromic_substring.py
+++ String/longest_palindromic_substring.py
@@ -2,27 +2,62 @@
 class Solution:
     # brute force: time O(n^2), space O(1)
     def longestPalindrome(self, s: str) -> str:
-        size = len(s)
+        n = len(s)
+        start = 0
+        max_len = 0
+
+        def extend_center(l: int, r: int):
+            nonlocal start, max_len
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            if max_len < r - l - 1:
+                max_len = r - l - 1
+                start = l + 1
+        
+        for i in range(n):
+            extend_center(i, i)
+            if i < n - 1:
+                extend_center(i, i + 1)
+        
+        return s[start: start + max_len]
+        
+    
+    # O(n^3) brute force
+    def longestPalindromeBF(self, s: str):
+        n = len(s)
         max_len = 0
         start = 0
-        for i in range(size):
-            j = 0
-            while i - j >= 0 and i + j < size and s[i - j] == s[i + j]:
-                if 2 * j + 1 > max_len:
-                    max_len = 2 * j + 1
-                    start = i - j
-                j += 1
-        
-        for i in range(1, size):
-            j = 0
-            while s[i - 1] == s[i] and i - 1 - j >= 0 and i + j < size and s[i - 1 - j] == s[i + j]:
-                if 2 * j + 2 > max_len:
-                    max_len = 2 * j + 2
-                    start = i - 1 - j
-                j += 1
-        
-        return s[start:start + max_len]
+        def is_palindrome(s: str) -> bool:
+            l, r = 0, len(s) - 1
+            while l < r:
+                if s[l] == s[r]:
+                    l += 1
+                    r -= 1
+                else:
+                    return False
+            
+            return True
+
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                if is_palindrome(s[i:j]):
+                    if max_len < j - i:
+                        max_len = j - i
+                        start = i
+
+        return s[start: start + max_len]
+
+
     
 s = Solution()
 print(s.longestPalindrome('babad'))
-            
+'''
+abc
+a
+ab
+abc
+b
+bc
+c
+'''
