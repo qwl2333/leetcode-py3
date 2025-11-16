1450f4d8042d318fb79009598b11306641a12969
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2025-11-15 22:40:14 -0800

    1762 buildings with an ocean view add comments

--- Companies/Meta/Stack/buildings-with-an-ocean-view.py
+++ Companies/Meta/Stack/buildings-with-an-ocean-view.py
@@ -5,7 +5,12 @@ class Solution:
         stack = []
         i = 0
         while i < len(heights):
-            while stack and heights[i] >= heights[stack[-1]]:
+            # 最难的就是这个>=等号, [4,2,2‘,1] 返回 [0,2,3]
+            # 意思是2是看不到海边的因为被2'挡住了
+            # 所以如果当前高度== stack最后一个元素的高度，需要把那个元素pop出来
+            # 加上等号是一个严格递减的单调栈
+            # 不加上等号是一个不严格递减的单调栈
+            while stack and heights[i] >= heights[stack[-1]]:                 
                 stack.pop()
             stack.append(i)
             i += 1
