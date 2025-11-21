de3f237f28202f99cde629b7ec71845dffb75265
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2025-11-21 01:03:31 -0800

    lc 636 exclusive time of functions better sol

--- Companies/Meta/Stack/exclusive-time-of-functions.py
+++ Companies/Meta/Stack/exclusive-time-of-functions.py
@@ -1,23 +1,29 @@
 # lc 636
 class Solution:
-    # T: O(n) n - len of logs, S: O(n)
+    # T: O(n), S: O(n)
     def exclusiveTime(self, n: int, logs: list[str]) -> list[int]:
-        res = [0 for i in range(n)]
-        stack = []
-        for log in logs:
-            id, operation, cur_time = log.split(':')
-            id, cur_time = int(id), int(cur_time)
-            if operation == 'start':
-                stack.append((id, cur_time))
-            else: # operation == 'end'
-                _id, cur_start_time = stack.pop()
-                time_taken = cur_time - cur_start_time + 1
-                res[id] += time_taken
+        res = [0] * n
+        stack = []           # 存当前在跑的函数 id
+        prev_time = 0        # 上一个时间点
 
+        for log in logs:
+            fid, typ, t = log.split(':')
+            fid, t = int(fid), int(t)
+
+            if typ == 'start':
                 if stack:
-                    caller_id, _caller_start_time = stack[-1]
-                    res[caller_id] -= time_taken
-        
+                    # 从 prev_time 到 t-1 都是栈顶在跑
+                    res[stack[-1]] += t - prev_time
+                stack.append(fid)
+                prev_time = t
+            else:  # 'end'
+                # 栈顶函数从 prev_time 跑到 t
+                res[stack[-1]] += t - prev_time + 1
+                stack.pop()
+                prev_time = t + 1  # 这些+1包括前面21行的+1 本质上 
+                                   # 开始的时间start 5，和end 5其实不是一个时间
+                                   # end 5 代表的是 start 6，看leetcode 例子的图片就知道了
+
         return res
 
 s = Solution()
