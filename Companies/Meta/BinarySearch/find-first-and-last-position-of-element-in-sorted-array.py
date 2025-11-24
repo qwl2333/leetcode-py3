944e98b1223ddc9091266aa58c9509ea8f85ebc1
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2025-11-23 21:37:10 -0800

    lc 34

--- Companies/Meta/BinarySearch/find-first-and-last-position-of-element-in-sorted-array.py
+++ Companies/Meta/BinarySearch/find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,41 +2,34 @@
 
 class Solution:
     def searchRange(self, nums: list[int], target: int) -> list[int]:
-        res = [-1, -1]
-        if not nums:
-            return res
-
-        def lower_bound(l: int, r: int) -> int: # first element >= target
+        def find_first_bigger_or_equal_to(nums: list[int], l: int, r: int, target: int) -> int:
             while l <= r:
                 mid = (l + r) // 2
                 if nums[mid] >= target:
                     r = mid - 1
                 else:
                     l = mid + 1
-                
+
+            if l == len(nums):
+                return -1
             return l
         
-        def upper_bound(l: int, r: int) -> int: # last element <= target
+        def find_last_smaller_or_equal_to(nums: list[int], l: int, r: int, target: int) -> int:
             while l <= r:
                 mid = (l + r) // 2
                 if nums[mid] > target:
                     r = mid - 1
                 else:
                     l = mid + 1
-                
-            return r
-        n = len(nums)
-        l = lower_bound(0, n - 1)
-        if l == n or nums[l] != target:
-            return res
-        else:
-            res[0] = l
-        
-        u = upper_bound(0, n - 1)
 
-        if u < 0 or nums[u] != target:
-            return res
+            if r == -1:
+                return -1
+            return r
+    
+        n = len(nums)
+        f = find_first_bigger_or_equal_to(nums, 0, n - 1, target)
+        l = find_last_smaller_or_equal_to(nums, 0, n - 1, target)
+        if f != -1 and l != -1 and nums[f] == target and nums[l] == target:
+            return [f, l]
         else:
-            res[1] = u
-        
-        return res
+            return [-1, -1]
