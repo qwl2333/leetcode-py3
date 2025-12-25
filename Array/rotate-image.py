3615aedaaa8d5eec57de890dcb4a0185082d62ae
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2025-12-25 00:32:31 -0800

    lc 48

--- Array/rotate-image.py
+++ Array/rotate-image.py
@@ -1,5 +1,28 @@
 # lc 48 要是忘记了，直接看neetcode的解法吧
 class Solution:
+    '''
+    这个解法最好记
+    transpose then reverse each row
+    TC O(N^2)
+    SC O(1)
+    '''
+    def rotate1(self, matrix: list[list[int]]) -> None:
+            """
+            Do not return anything, modify matrix in-place instead.
+            """
+            n = len(matrix)
+            
+            # Step 1: Transpose (swap matrix[i][j] with matrix[j][i])
+            # We only iterate through the upper triangle (j > i) to avoid double-swapping
+            for i in range(n):
+                for j in range(i + 1, n):
+                    print(f'i:{i}, j:{j}')
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
+            # Step 2: Reverse each row
+            for i in range(n):
+                matrix[i].reverse()
+
     # Time O(n^2), Space O(1)
     '''
     一圈一圈的从外往内rotate
@@ -25,5 +48,5 @@ input = [[5,1,9,11],
          [2,4,8,10],
          [13,3,6,7],
          [15,14,12,16]]
-s.rotate(input)
+s.rotate1(input)
 print(input)
