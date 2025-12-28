9de1e77bb601764aef0df8862e253b99a6f2a561
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2025-12-28 14:58:20 -0800

    lc 398

--- Companies/Meta/HashMap/random-pick-index.py
+++ Companies/Meta/HashMap/random-pick-index.py
@@ -3,7 +3,7 @@
 import random
 from collections import defaultdict
 class Solution:
-    # 肯定是这个好，第二个Reservoir Sampling纯纯炫技
+    # 肯定是这个好，第二个Reservoir Sampling 适用于 nums 太大了 以至于内存存不下 indices 这样的dict, 或者 数组是一个stream
     # t O(n), s O(n)
     def __init__(self, nums: list[int]):
         self.indices = defaultdict(list)
@@ -16,6 +16,9 @@ class Solution:
         random_pick = random.randint(0, len(indices_for_target) - 1)
         return indices_for_target[random_pick]
 
+'''
+如果数组非常大（大到内存装不下索引字典），或者数组是一个流（Stream）（一直在增加，无法预处理），那么只有水塘抽样能解决问题，因为它不需要额外的内存来存储位置信息
+'''
 class SolutionReservoirSampling:
     # t O(1) s O(n)
     def __init__(self, nums: list[int]):
