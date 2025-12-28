e13b1e16d1513c7e87e3aa7a5372f0a325bf872b
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2025-12-27 23:08:18 -0800

    lc 249

--- Companies/Meta/HashMap/group-shifted-strings.py
+++ Companies/Meta/HashMap/group-shifted-strings.py
@@ -2,6 +2,17 @@
 class Solution:
     # T: O(m*n) n - length of strings, m - max size of the string in strings
     # S: O(m*n)
+    '''
+    在 Python 中：-1 % 26 = 25
+    在 C# / Java / C++ 中：-1 % 26 = -1
+    在 C# 中获得正数余数的通用公式
+    int result = ((a % n) + n) % n;
+
+    // 例子：
+    // ((-1 % 26) + 26) % 26 
+    // = (-1 + 26) % 26 
+    // = 25 % 26 = 25
+    '''
     def groupStrings(self, strings: list[str]) -> list[list[str]]:
         key_to_strings = {}
         for s in strings:
@@ -9,10 +20,13 @@ class Solution:
             for i in range(len(s) - 1):
                 diff = ord(s[i + 1]) - ord(s[i])
                 '''
+                 python 不同于c# 特有的
                  ba: -1 % 26 = 25
                  az: 25 % 26 = 25
+                 为了通用一点也可以
+                    ((diff % 26) + 26) % 26 instead of (diff % 26)
                 '''
-                key += f'{(diff % 26)},'
+                key += f'{(diff % 26)},' 
             if key not in key_to_strings:
                 key_to_strings[key] = []
             key_to_strings[key].append(s)
@@ -20,4 +34,4 @@ class Solution:
         return list(key_to_strings.values())
 
 s = Solution()
-print(s.groupStrings(["abc","bcd","acef","xyz","az","ba","a","z"]))
+print(s.groupStrings(["acb","bcd","acef","xyz","az","ba","a","z"]))
