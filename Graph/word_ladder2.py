889a9d064b9b6ec996bdbb28772832d83d9c5266
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2026-01-15 03:13:10 -0800

    126 127 word ladder 1 & 2

--- Graph/word_ladder2.py
+++ Graph/word_ladder2.py
@@ -8,7 +8,7 @@ class Solution:
         visited = {} # key visited_word, value - 从begin_word出发到visited word最短距离，也就是level order traversal的level
         adjacent_list = defaultdict(list)
 
-        def bfs(begin_word: str, end_word: str) -> int: # bfs find the target and populate a adjacent list
+        def bfs(begin_word: str, end_word: str) -> None: # bfs find the target and populate a adjacent list
             queue = deque([begin_word])
             if end_word not in word_set:
                 return
@@ -19,11 +19,14 @@ class Solution:
                 number_of_nodes_at_current_level = len(queue) # number of nodes at current level
                 for _ in range(number_of_nodes_at_current_level):
                     cur_word = queue.popleft()
+                    if cur_word == end_word: # 这里表面end_word上一层已经走完了,那上一层的word能到end_word的表信息和距离信息都有了
+                        return
                     for i in range(len(cur_word)):
                         for letter in alphabet:
                             new_word = cur_word[0:i] + letter + cur_word[i+1:]
                             print(new_word)
                             if new_word in word_set and new_word != cur_word:
+                            # if new_word not in visited or visited[new_word] == visited[cur_word] + 1: 加这一行可以只记录“向前方”走的边，把回头路和横向路挡在门外
                                 adjacent_list[cur_word].append(new_word)
                                 if new_word not in visited:
                                     queue.append(new_word)
@@ -49,4 +52,4 @@ class Solution:
 
 
 s = Solution()
-print(s.findLadders('hit', 'cog', ["hot","dot","dog","lot","log","cog"]))
+print(s.findLadders('red', 'tax', ['ted', 'rex', 'tex', 'tax']))
