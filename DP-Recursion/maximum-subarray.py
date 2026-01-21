2639d6ff2c91f9fcf41574a9b839d631297aa404
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2026-01-21 01:46:48 -0800

    lc 53

--- DP-Recursion/maximum-subarray.py
+++ DP-Recursion/maximum-subarray.py
@@ -6,6 +6,21 @@ class Solution:
         dp[0] = nums[0]
         max_sum = dp[0]
         for i in range(1, n):
-            dp[i] = max(nums[i], nums[i] + dp[i - 1])
+            if dp[i - 1] > 0:
+                dp[i] = nums[i] + dp[i - 1]
+            else:
+                dp[i] = nums[i]
             max_sum = max(max_sum, dp[i])
         return max_sum
+
+        # 再简化到不需要dp array
+        # n = len(nums)
+        # pre_sum = nums[0]
+        # max_sum = nums[0]
+        # for i in range(1, n):
+        #     if pre_sum > 0:
+        #         pre_sum = nums[i] + pre_sum
+        #     else:
+        #         pre_sum = nums[i]
+        #     max_sum = max(max_sum, pre_sum)
+        # return max_sum
