a3408a0ff380a8ff5e8e9074df8fd022c187a04c
qwl2333 <120740141+qwl2333@users.noreply.github.com>
2026-01-25 23:43:41 -0800

    lc 1047

---
+++ Companies/Meta/Stack/remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,30 @@
+# lc 1047
+class Solution:
+    # ai的sol就是比较elegant
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for c in s:
+            if stack and stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+        
+        return ''.join(stack)
+
+    # 我的sol比较笨比
+    def removeDuplicatesMySol(self, s: str) -> str:
+        stack = []
+        size = len(s)
+        i = 0
+        while i < size:
+            if not stack:
+                stack.append(s[i])
+                i += 1
+                continue
+            while i < size and stack and stack[-1] == s[i]:
+                stack.pop()
+                i += 1
+            if i < size:
+                stack.append(s[i])
+                i += 1
+        return ''.join(stack)
